787b2ab2207dec0a5f0ed85a3dc25598190060d2
Ravi <84305482+Ravi9985@users.noreply.github.com>
2021-10-11 21:08:35 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -2,13 +2,13 @@
   public static void main(String args[]){
     System.out.println("Hello Java");
   }
-class Add {
+class sub {
 
   // create a method
   public int addNumbers(int a, int b) {
-    int sum = a + b;
+    int sub = a - b;
     // return value
-    return sum;
+    return sub;
   }
 
 }
